bb6b74c61a1c7f18fff3726f516d8ca2f99982eb
Solange <solange.le-luel@epitech.eu>
2026-06-02 17:56:15 +0200

    :fire: Remove useless imports

--- src/main/java/com/example/todolists/domain/todo/service/ToDoService.java
+++ src/main/java/com/example/todolists/domain/todo/service/ToDoService.java
@@ -1,13 +1,8 @@
 package com.example.todolists.domain.todo.service;
 
 import com.example.todolists.domain.todo.dto.ToDoResponse;
-import com.example.todolists.security.UserDetails;
 import com.example.todolists.domain.todo.dto.CreateToDoDto;
 import com.example.todolists.domain.todo.dto.UpdateToDoDto;
-import com.example.todolists.exception.ResourceNotFoundException;
-import com.example.todolists.domain.todo.entity.ToDo;
-import com.example.todolists.domain.todo.repository.ToDoRepository;
-import org.springframework.security.authorization.AuthorizationDeniedException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
